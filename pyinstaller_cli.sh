48a7fd008e8c6c3d0663678b5c2d5eb7ead7c4f5
Andy Hou <andyhou965@gmail.com>
2022-11-17 21:02:39 +0400

    Update the configurations

--- pyinstaller_cli.sh
+++ pyinstaller_cli.sh
@@ -2,4 +2,4 @@ pyinstaller main.py\
     --noconsole\
     --add-data ./conf:conf --add-data ./videos:videos --add-data ./images:images --add-data ./known-faces:known-faces --add-data ./name-cards:name-cards\
     --icon="app-icon.png"\
-    --name NEXT50-AR
+    --name NEXT-AR
